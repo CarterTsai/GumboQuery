1394720405c25d0b81e20cd874eea53e3a7b9116
Carter Tsai <hamming1@gmail.com>
2013-11-16 13:58:34 +0800

    Add new testing items, but class no implement that function

--- unit_test/test.py
+++ unit_test/test.py
@@ -10,15 +10,25 @@ class GumboPlusTest(unittest.TestCase):
     def setUp(self):
         testHtml = '<h1 class="ss cc">123</h1>' \
                    '<div class="ss">456<div>' \
-                   '<h1 class="ss" title="cc" >345</h1>'
-        self.parser = GumboPlus(testHtml)
+                   '<h1 class="ss" title="cc" >345</h1>' \
+                   '<div class="myClass" title="good" >345</h1>' \
+                   '<div id="myID" title="best" >Good Idea</h1>'
+        self.data = GumboPlus(testHtml)
+
+    def test_query_class(self):
+        x = self.data.query('.myClass')
+        self.assertEqual(x[0].contents[0], unicode('test123'))
+
+    def test_query_id(self):
+        x = self.data.query('#myID')
+        self.assertEqual(x[0].contents[0], unicode('Good Idea'))
 
     def test_find_class(self):
-        x = self.parser.findClass('h1', 'ss')
+        x = self.data.findClass('h1', 'ss')
         self.assertEqual(x[0].contents[0], unicode('123'))
 
     def test_find_title(self):
-        x = self.parser.findTitle('h1', 'cc')
+        x = self.data.findTitle('h1', 'cc')
         self.assertEqual(x[0].contents[0], unicode('345'))
 
 if __name__ == '__main__':
